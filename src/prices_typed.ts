25bbc18fc72e72abf056037375f2dc4a1522fa61
Yosef <yosefetene02@gmail.com>
2025-02-13 10:55:58 +0300

    Added date2 to calculateCost

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -1,6 +1,7 @@
 import "./polyfills";
 import express from "express";
 import { Database } from "./database";
+import { Temporal } from "@js-temporal/polyfill";
 
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
@@ -20,17 +21,25 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string);
-    const cost = calculateCost(age, type, date, baseCost);
+    const date2 = parsePlainDate(req.query.date as string);
+    const cost = calculateCost(age, type, date, baseCost, date2);
     res.json({ cost });
   });
 
   function parseDate(dateString: string | undefined): Date | undefined {
     if (dateString) {
-      return new Date(dateString);
+      return  new Date(dateString);
     }
   }
 
-  function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number) {
+
+  function parsePlainDate(dateString: string | undefined): Temporal.PlainDate | undefined {
+    if (dateString) {
+      return  Temporal.PlainDate.from(dateString);
+    }
+  }  
+
+  function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number, date2: Temporal.PlainDate | undefined) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
